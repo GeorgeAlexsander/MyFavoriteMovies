af6c2c86a4e9424cdda1111e98f9d80f30fa9594
GeorgeAlexsander <georgealexsanderflores@gmail.com>
2022-08-28 18:07:12 -0300

    Implementado funcionalidade de favoritar filmes

--- MyFavoriteFilms/main.js
+++ MyFavoriteFilms/main.js
@@ -2,11 +2,78 @@
 
 const moviesContainer = document.querySelector('.movies')
 
+const input = document.querySelector('input')
+const searchButton = document.querySelector('.searchIcon');
+
+searchButton.addEventListener('click', managersearchSystem)
+
+input.addEventListener("keyup", function(event){
+    event.preventDefault();
+    if (event.key === "Enter")
+        managersearchSystem()
+})
+
+async function managersearchSystem() {
+    const inputValue = input.value
+    if (inputValue != '') {
+        cleanAllMovies()
+        const movies = await getFilmeEspecifico(inputValue)
+        movies.forEach(movie => renderMovie(movie))
+    }
+}
+
+function cleanAllMovies() {
+  moviesContainer.innerHTML = ''
+}
 
 async function getFilmesPopulares() {
     let response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${ChaveApi}&language=pt-BR-US&page=1`);
     let { results } = await response.json();
     return results;
+}
+
+async function getFilmeEspecifico(FilmeSelecionado) {
+    let response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${ChaveApi}&language=pt-BR&query=${FilmeSelecionado}&page=1`);
+    let { results } = await response.json();
+    return results;
+}
+
+function getFavoriteMovies() {
+    return JSON.parse(localStorage.getItem('favoriteMovies'))
+  }
+
+function checkMovieIsFavorited(id) {
+    const movies = getFavoriteMovies() || [] 
+    return movies.find(movie => movie.id == id)
+}
+
+function saveToLocalStorage(movie) {
+    const movies = getFavoriteMovies() || []
+    movies.push(movie)
+    localStorage.setItem('favoriteMovies', JSON.stringify(movies))
+}
+
+function removeToLocalStorage(id) {
+    const movies = getFavoriteMovies() || []
+    const findMovie = movies.find(movie => movie.id == id)
+    const newMovies = movies.filter(movie => movie.id != findMovie.id)
+    localStorage.setItem('favoriteMovies', JSON.stringify(newMovies))
+}
+
+function favoriteButtonStatus(event, movie) {
+    const favoriteStatus = {
+        favorited: 'images/heart-fill.svg',
+        notFavorited: 'images/heart.svg'
+    }
+
+    if(event.target.src.includes(favoriteStatus.notFavorited)) {
+        event.target.src = favoriteStatus.favorited
+        saveToLocalStorage(movie)
+    }
+    else if(event.target.src.includes(favoriteStatus.favorited)) { 
+        event.target.src = favoriteStatus.notFavorited
+        removeToLocalStorage(movie.id)
+    }
 }
 
 window.onload = async function() {
@@ -15,10 +82,11 @@ window.onload = async function() {
 }
 
 function renderMovie(movie) {
-    const { title, poster_path, vote_average, release_date, overview} = movie
+    const { id, title, poster_path, vote_average, release_date, overview} = movie
+    
     const year = new Date(release_date).getFullYear()
     const image = 'https://image.tmdb.org/t/p/w500'+poster_path
-    const isFavorited = false
+    const isFavorited = checkMovieIsFavorited(id)
 
     const movieElement = document.createElement('div')
     movieElement.classList.add('movie')
@@ -64,6 +132,7 @@ function renderMovie(movie) {
     favoriteImage.src = isFavorited ? 'images/heart-fill.svg' : 'images/heart.svg'
     favoriteImage.alt = 'Heart'
     favoriteImage.classList.add('favoriteImage')
+    favoriteImage.addEventListener('click', (event) => favoriteButtonStatus(event, movie))
 
     const favoriteText = document.createElement('span')
     favoriteText.classList.add('movie-favorite')
